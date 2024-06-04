c578bd8af8ad9801534ece25d1c545894e0ebeb1
unknown <hycute.ts@gmail.com>
2024-06-04 20:59:02 +0600

    create a post request for adding user in database

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
-
 
 const Username = process.env.DATABASE_ACCESS_USERNAME
 const Password = process.env.DATABASE_ACCESS_PASSWORD
 
-
+app.use(express.json());
+app.use(cors({
+origin: ["http://localhost:5173"]
+}))
 
 const uri = `mongodb+srv://${Username}:${Password}@cluster0.zukg64l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,9 +28,28 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const redLoveUserCollection = client.db("RedLove").collection("User")
+
+
+
+    app.post("/all-users", async (req, res)=>{
+        const UserData = req.body;
+        console.log(UserData);
+        const result = await redLoveUserCollection.insertOne(UserData)
+        res.send(result);
+    })
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server responsed")
+});
+
+app.listen(port, () => {
+  console.log(`surver running on port: ${port}`);
+});
