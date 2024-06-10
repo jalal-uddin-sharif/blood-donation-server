017703d18e0e5ff0bc48a61265ff718df1d93488
unknown <hycute.ts@gmail.com>
2024-06-10 16:48:34 +0600

    create verify token, veryfyadmin token and verify adminvolunteer token with jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const Username = process.env.DATABASE_ACCESS_USERNAME;
@@ -11,10 +12,24 @@ const Password = process.env.DATABASE_ACCESS_PASSWORD;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://red-love-donation.web.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 );
+
+//middleware
+const verifyToken = (req, res, next) => {
+  console.log(req.headers);
+  if (!req.headers.authorization)
+    return res.status(401).send({ message: "unauthorized access" });
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.API_SECRET_KEY, (err, decoded) => {
+    if (err) return res.status(401).send({ message: "unauthorized access" });
+    req.decoded = decoded;
+    next();
+  });
+};
+
 
 const uri = `mongodb+srv://${Username}:${Password}@cluster0.zukg64l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
@@ -33,21 +48,66 @@ async function run() {
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const redLoveUserCollection = client.db("RedLove").collection("User");
-    const redLoveBlogCollection = client.db("RedLove").collection("BlogsCollection");
+    const redLoveBlogCollection = client
+      .db("RedLove")
+      .collection("BlogsCollection");
     const redLoveRegisteredDonation = client
       .db("RedLove")
       .collection("createdDonation");
 
+  //verify admin
+const verifyAdmin = (req, res, next) => {
+  if (!req.headers.authorization)
+    return res.status(401).send({ message: "Unauthorized access" });
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.API_SECRET_KEY, async(err, decoded) => {
+    if (err) return res.status(401).send({ message: "Unauthorized access" });
+    const user = await redLoveUserCollection.findOne({Email: decoded.email})
+    if(user?.Role === "Admin"){
+      next()
+    } else{
+      return res.status(403).send({ message: "forbidden access" })
+     }
+  });
+};
+
+  //verify admin and volunteer
+const verifyAdminVolunteer = (req, res, next) => {
+  if (!req.headers.authorization)
+    return res.status(401).send({ message: "Unauthorized access" });
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.API_SECRET_KEY, async(err, decoded) => {
+    if (err) return res.status(401).send({ message: "Unauthorized access" });
+    const user = await redLoveUserCollection.findOne({Email: decoded.email})
+    if(user?.Role === "Admin" || user?.Role === "Volunteer"){
+      next()
+    } else{
+     return res.status(403).send({ message: "forbidden access" })
+    }
+  });
+};
+
+    //jwt token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.API_SECRET_KEY, {
+        expiresIn: "365d",
+      });
+      res.send({ token });
+    });
+
     //add user from registration page
     app.post("/all-users", async (req, res) => {
       const UserData = req.body;
-
       const result = await redLoveUserCollection.insertOne(UserData);
       res.send(result);
     });
 
     //create donation request
-    app.post("/new-donation-request", async (req, res) => {
+    app.post("/new-donation-request", verifyToken, async (req, res) => {
       const registeredDonation = req.body;
       const result = await redLoveRegisteredDonation.insertOne(
         registeredDonation
@@ -56,7 +116,7 @@ async function run() {
     });
 
     //get donation requests
-    app.get("/my-donation-request/:email", async (req, res) => {
+    app.get("/my-donation-request/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
 
       const result = await redLoveRegisteredDonation
@@ -66,7 +126,8 @@ async function run() {
     });
 
     //get recent requests
-    app.get("/my-recent-donation/:email", async (req, res) => {
+    app.get("/my-recent-donation/:email", verifyToken, async (req, res) => {
+      console.log(req.headers);
       const email = req.params.email;
       const result = await redLoveRegisteredDonation
         .find({ requesterEmail: email })
@@ -77,7 +138,7 @@ async function run() {
     });
 
     //get all users for admin only
-    app.get("/all-users", async (req, res) => {
+    app.get("/all-users", verifyAdmin, async (req, res) => {
       const result = await redLoveUserCollection.find().toArray();
       res.send(result);
     });
@@ -99,7 +160,6 @@ async function run() {
     app.patch("/update-user-status", async (req, res) => {
       const email = req.query.email;
       const newStatus = req.body.status;
-      console.log(email, newStatus);
 
       const result = await redLoveUserCollection.findOneAndUpdate(
         { Email: email },
@@ -117,7 +177,7 @@ async function run() {
     });
 
     //update user profile
-    app.put("/update-user-profile/:email", async (req, res) => {
+    app.put("/update-user-profile/:email", verifyToken, async (req, res) => {
       const userData = req.body;
       const email = { Email: req.params.email };
       const options = { upsert: true };
@@ -135,13 +195,13 @@ async function run() {
     });
 
     //get all blood donation request
-    app.get("/all-blood-donation-request", async (req, res) => {
+    app.get("/all-blood-donation-request", verifyAdminVolunteer, async (req, res) => {
       const result = await redLoveRegisteredDonation.find().toArray();
       res.send(result);
     });
 
     //update donation request
-    app.put("/update-donation-request/:id", async (req, res) => {
+    app.put("/update-donation-request/:id", verifyToken, async (req, res) => {
       const newReq = req.body;
 
       const result = await redLoveRegisteredDonation.findOneAndUpdate(
@@ -152,94 +212,140 @@ async function run() {
       res.send({ success: true, result });
     });
 
-
     //get request data by id
-    app.get("/get-request-data/:id", async (req, res) => {
-      console.log(req.params.id);
-      const result = await redLoveRegisteredDonation.findOne({_id: new ObjectId(req.params.id)});
+    app.get("/get-request-data/:id", verifyToken, async (req, res) => {
+      const result = await redLoveRegisteredDonation.findOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(result);
     });
 
     //create blog
-    app.post("/create-new-blog", async(req, res)=>{
-      const blogData = req.body
-      const result = await redLoveBlogCollection.insertOne(blogData)
-      res.send(result)
-    })
+    app.post("/create-new-blog", verifyAdminVolunteer, async (req, res) => {
+      const blogData = req.body;
+      const result = await redLoveBlogCollection.insertOne(blogData);
+      res.send(result);
+    });
 
     //get all blogs
-    app.get("/all-blogs", async(req, res)=>{
-      const status = req.query.status
-      const query = {}
-      if(status){
-        query.status = status
+    app.get("/all-blogs", async (req, res) => {
+      const status = req.query.status;
+      const query = {};
+      if (status) {
+        query.status = status;
       }
-      console.log(status);
-      const result = await redLoveBlogCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await redLoveBlogCollection.find(query).toArray();
+      res.send(result);
+    });
 
     //update blog status
-    app.patch("/update-blog-status/:id", async(req, res)=>{
-     const id = req.params.id
-     const status = req.query.status
-     const result = await redLoveBlogCollection.findOneAndUpdate(
-      { _id: new ObjectId(id) },
-      { $set: { status: status } },
-      { returnDocument: "after" }
-     )
-    //  console.log(id, status);
-    res.send(result);
-    })
+    app.patch("/update-blog-status/:id", verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const status = req.query.status;
+      const result = await redLoveBlogCollection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { status: status } },
+        { returnDocument: "after" }
+      );
+      res.send(result);
+    });
 
     //delete blog
-    app.delete("/delete-blog/:id", async(req, res)=>{
-      const result = await redLoveBlogCollection.deleteOne({_id: new ObjectId(req.params.id)})
-      res.send(result)
-    })
-//update donation status
-app.patch("/update-donation-status/:id", async(req, res)=>{
-  const id = req.params.id
-  const donationStatus = req.query.status
-  const result = await redLoveRegisteredDonation.findOneAndUpdate(
-    { _id: new ObjectId(id) },
-    { $set: { donationStatus: donationStatus } },
-    { returnDocument: "after" }
-  )
-  res.send(result)
-})
-//search donors
-app.get("/search-donors", async(req, res)=>{
-  const {bloodGroup, district, upazila} =req.query
-  const query = {
-    bloodGroup, district , upazila
-  }
-  const result = await redLoveRegisteredDonation.find(query).toArray()
-  res.send(result)
-})
+    app.delete("/delete-blog/:id", verifyAdmin, async (req, res) => {
+      const result = await redLoveBlogCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
 
-//get pending donation data
-app.get("/pending-donation-data", async(req, res)=>{
-  const result = await redLoveRegisteredDonation.find({
-    donationStatus: "pending"}).toArray()
-    res.send(result)
-})
+    //update donation status
+    app.patch("/update-donation-status/:id", verifyAdminVolunteer, async (req, res) => {
+      const id = req.params.id;
+      const donationStatus = req.query.status;
+      const result = await redLoveRegisteredDonation.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { donationStatus: donationStatus } },
+        { returnDocument: "after" }
+      );
+      res.send(result);
+    });
 
-//view donation details
-app.get("/view-details/:id",async(req, res)=>{
-  console.log(req.params.id);
-  const result = await redLoveRegisteredDonation.findOne({_id: new ObjectId(req.params.id)})
-  res.send(result)
-})
+    //update donation status by user
+    app.patch("/user-donation-status-update/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const donationStatus = req.query.status;
+      const result = await redLoveRegisteredDonation.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { donationStatus: donationStatus } },
+        { returnDocument: "after" }
+      );
+      res.send(result);
+    });
 
-//get all published blogs
-app.get("/blogs", async(req, res)=>{
-  const query = {status: "published"}
-  const result = await redLoveBlogCollection.find(query).toArray()
-  res.send(result)
-})
+    //search donors
+    app.get("/search-donors", async (req, res) => {
+      const { bloodGroup, district, upazila } = req.query;
+      const query = {
+        bloodGroup,
+        district,
+        upazila,
+      };
+      const result = await redLoveRegisteredDonation.find(query).toArray();
+      res.send(result);
+    });
 
+    //get pending donation data
+    app.get("/pending-donation-data", async (req, res) => {
+      const result = await redLoveRegisteredDonation
+        .find({
+          donationStatus: "pending",
+        })
+        .toArray();
+      res.send(result);
+    });
 
+    //view donation details
+    app.get("/view-details/:id", verifyToken, async (req, res) => {
+      const result = await redLoveRegisteredDonation.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    //get all published blogs
+    app.get("/blogs", async (req, res) => {
+      const query = { status: "published" };
+      const result = await redLoveBlogCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //get details of blog
+    app.get("/blog-details/:id", async (req, res) => {
+      const result = await redLoveBlogCollection.findOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    //update donor information
+    app.patch("/confirm-donation/:id", async (req, res) => {
+      const { donorName, donorEmail, donationStatus } = req.body;
+      const id = req.params.id;
+      const result = await redLoveRegisteredDonation.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { donorName, donorEmail, donationStatus } },
+        { upsert: true, returnOriginal: false }
+      );
+      res.send(result);
+    });
+
+    //delete donation request
+    app.delete("/delete-donation-request/:id", async (req, res) => {
+      const result = await redLoveRegisteredDonation.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
